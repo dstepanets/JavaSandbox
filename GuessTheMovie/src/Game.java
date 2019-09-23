995f5ce0a59e92deab1f9ebbcbd7596c7304be23
dstepanets <dm.stepanets@gmail.com>
2019-09-24 02:33:48 +0300

    debugging left

--- GuessTheMovie/src/Game.java
+++ GuessTheMovie/src/Game.java
@@ -1,19 +1,62 @@
-
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class Game {
 
-	private char[] answer;
-	private char[] question;
+	private String			answer;
+	private char[]			guess;
+	private String			wrongLetters;
+	private boolean			win;
 
 	Game(String a) {
-		answer = a.toLowerCase().toCharArray();
-//		question = a.
+		answer = a.toLowerCase();
+		guess = new char[a.length()];
+		Arrays.fill(guess, '_');
+		win = false;
+		wrongLetters = new String();
 	}
 
 	public void runGame() {
 
+		System.out.println("Hey, pal! Guess my favourite movie.");
+		System.out.println("Its name has " + answer.length() + " letters.");
 
+		while (!win && wrongLetters.length() < 10) {
+			printQuestion();
+			getInput();
+			if (new String(guess).equals(answer))
+				win = true;
+		}
+		if (wrongLetters.length() >= 10)
+			System.out.println("YOU LOST :(");
+		else {
+			System.out.println("Movie name: " + new String(guess));
+			System.out.println("\tYOU WON!!! :D");
+		}
+	}
+
+	private void printQuestion() {
+		System.out.println("You have guessed (" + wrongLetters.length() + "/10) wrong letters: " + wrongLetters);
+		System.out.println("Movie name: " + new String(guess));
+	}
+
+	private void getInput() {
+
+		Scanner input = new Scanner(System.in);
+
+		System.out.print("Give me a letter: ");
+		char c = input.nextLine().toLowerCase().charAt(0);
+		int i = answer.indexOf(c);
+		if (i == -1 && (wrongLetters.indexOf(c) == -1))
+			wrongLetters += c;
+		else {
+			while (i > -1) {
+				guess[i] = c;
+				i = answer.indexOf(c, i + 1);
+			}
+		}
 
 	}
+
 
 }
--- GuessTheMovie/src/Main.java
+++ GuessTheMovie/src/Main.java
@@ -1,39 +1,1 @@
-
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Scanner;
-
-public class Main {
-
-	static private ArrayList<String> movies = new ArrayList<>();
-
-    public static void main(String[] args) {
-
-    	try {
-
-			File file = new File("movies.txt");
-			Scanner reader = new Scanner(file);
-
-			while (reader.hasNextLine()) {
-				movies.add(reader.nextLine());
-			}
-
-			int index = (int)(Math.random() * movies.size());
-			System.out.println(index);
-			String answer = movies.get(index);
-			System.out.println(answer);
-
-			Game game = new Game(answer);
-
-//			for (String m : movies) {
-//				System.out.println(m);
-//			}
-
-
-		}  catch (IOException e) {
-			System.out.println("ERROR. Can't find or read file 'movies.txt'");
-		}
-    }
-
-}
+import java.io.File;import java.io.IOException;import java.util.ArrayList;import java.util.Scanner;public class Main {	static private ArrayList<String> movies = new ArrayList<>();    public static void main(String[] args) {    	try {			File file = new File("movies.txt");			Scanner reader = new Scanner(file);			while (reader.hasNextLine()) {				movies.add(reader.nextLine());			}			int index = (int)(Math.random() * movies.size());//			System.out.println(index);			String answer = movies.get(index);			System.out.println(answer);			Game game = new Game(answer);			game.runGame();		}  catch (IOException e) {			System.out.println("ERROR. Can't find or read file 'movies.txt'");		}    }}
